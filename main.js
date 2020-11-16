75652e95378e6f0aeaf5effa81691b77c8e5db81
nblock <blochowitz.noah@gmail.com>
2020-11-15 21:20:38 -0700

    created arrays of mad lib words

---
+++ main.js
@@ -0,0 +1,10 @@
+const adjective = ['abundant','slow','skinny','grim','hot','soft']
+const noun = ['lettuce','cow','dog','deck','door','stake']
+const typeOfBird = ['goose','duck','crow','sparrow','dodo']
+const roomInAHouse = ['office','kitchen','bedroom','bathroom']
+const verbPastTense = ['golfed','hid','clung','tripped','screamed','opended']
+const verb = ['run','scare','tease','fall','shovel','look']
+
+const findWord = type => {
+    
+}
